b3b368feeffdbb0d4f4e6b224ebd85529efd8f48
Gokul <146622216+0smic@users.noreply.github.com>
2025-05-08 17:27:21 +0530

    Update sample.sql

--- sample.sql
+++ sample.sql
@@ -1,7 +1,7 @@
-"""
-  sql is not type sensitive
-    it doesn#t care about it, even it is in syntax
-"""
+/*
+  sql keywords are not case sensitive
+    maybe table name and db be case sensitive in linux not in windows
+*/
 
 CREATE TABLE Students (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -13,18 +13,21 @@ CREATE TABLE Students (
 
 CREATE TABLE Student (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  hel VARCHAR(100),
+  name VARCHAR(100),
   age INT,
   salary DECIMAL(10,2)
 );
 
-SHOW TABLES;  #List all the table
-SHOW CREATE TABLE Students; --this will show how the table is created in first--
-DESCRIBE students;  -- list all the column in the table and there data type all the details--
-SELECT NOW();  -- This will show the current date --
-DROP TABLE Students;  -- This will delte the table---
-SHOW TABLES;  -- list all the table---
-ALTER TABLE student ADD hel VARCHAR(100);  -- Used to create a column in the table--
+SHOW TABLES;   -- List all the table
+SHOW CREATE TABLE Students; -- this will show how the table is created in first
+DESCRIBE students;  -- list all the column in the table and there data type all the details
+SELECT NOW();  -- This will show the current date 
+DROP TABLE Students;  -- This will delte the table
+SHOW TABLES;  -- list all the table
+
+ALTER TABLE student ADD hel VARCHAR(100);  -- Used to create a column in the table
 ALTER TABLE student DROP hel;  -- Used to remove column in the table
+
+
 INSERT INTO student (name,age,salary) VALUES ("JOHN",18,232.23); -- insert data into the table --
 SELECT * FROM student;  -- list all the data in the student table --
